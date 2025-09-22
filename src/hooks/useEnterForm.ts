923afd193b2a80f4b773711ed46b04872fc5384b
asilbek_tursun <148640148+AsilbekTursunov@users.noreply.github.com>
2025-09-22 11:32:45 +0500

    change login register url

--- src/hooks/useEnterForm.ts
+++ src/hooks/useEnterForm.ts
@@ -1,20 +1,21 @@
 import { useMutation } from "@tanstack/react-query"
 import { useAppDispatch } from "./useStoreSelector"
 import { setUser } from "../store/user.slice"
+import $axios from "@/lib/axios"
 
 
 export const useEnterForm = ({ url, method }: { url: string, method: string }) => {
   const dispatch = useAppDispatch()
   return useMutation({
     mutationFn: async (data: any) => {
-      const response = await fetch(`/api/auth/${url}?populate=*`, {
+      const response = await $axios.post(`/auth/${url}?populate=*`, {
         method: method,
         headers: {
           'Content-Type': 'application/json'
         },
         body: JSON.stringify(data)
       })
-      const result = await response.json()
+      const result = await response.data
       localStorage.setItem('token', result.jwt)
       return result
     },
